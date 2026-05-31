bd59a43961c6e70aa5d87199c127a6491c007dc3
starterDev3 <crazyfam@outlook.com>
2026-05-31 17:02:43 -0500

    eeeee

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,48 @@
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import Navbar from './components/Navbar';
+import { navLinks } from '../constants/index.js';
+import { useGSAP } from '@gsap/react';
+
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 
-const App = () => {
+const Navbar = () => {
+useGSAP(() => {
+  const navTween = gsap.timeline({
+    scrollTrigger: {
+      trigger: "nav",
+      start: "bottom top",
+    }
+  });
+
+  navTween.fromTo('nav', { backgroundColor: 'transparent'},{
+    backgroundColor: '#00000050',
+    backFilter: 'blur(10px)',
+    duration: 0.5,
+    ease: 'power1.inOut',
+
+  });
+})
+
   return (
   <nav>
     <div>
-      
+      <a href="#home" className="flex items-center gap-2">
+<img src="/images/logo.png" alt="logo" />
+<p>AGAHAGAGGGAGAGAAGA</p>
+      </a>
+      <ul>
+        {navLinks.map((link) => (
+          <li key={link.id}>
+            <a href={`#${link.id}`}>{link.title}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   </nav>
   );
 }
 
-export default App;
+export default Navbar;
