7f3ad853e4f572a47f546ae52923e1e55b5946a2
starterDev3 <crazyfam@outlook.com>
2026-06-02 16:36:11 -0500

    epic animations real!!!111!1!!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,18 @@
+import Navbar from './components/Navbar';
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
-import Navbar from './components/Navbar';
-import { navLinks } from '../constants/index.js';
-import { useGSAP } from '@gsap/react';
-
+import Hero from './components/Hero';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
-
-const Navbar = () => {
-useGSAP(() => {
-  const navTween = gsap.timeline({
-    scrollTrigger: {
-      trigger: "nav",
-      start: "bottom top",
-    }
-  });
-
-  navTween.fromTo('nav', { backgroundColor: 'transparent'},{
-    backgroundColor: '#00000050',
-    backFilter: 'blur(10px)',
-    duration: 0.5,
-    ease: 'power1.inOut',
-
-  });
-})
-
+const App = () => {
   return (
-  <nav>
-    <div>
-      <a href="#home" className="flex items-center gap-2">
-<img src="/images/logo.png" alt="logo" />
-<p>AGAHAGAGGGAGAGAAGA</p>
-      </a>
-      <ul>
-        {navLinks.map((link) => (
-          <li key={link.id}>
-            <a href={`#${link.id}`}>{link.title}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </nav>
-  );
+<main>
+  <Navbar />
+  <Hero />
+  <div className="h-dvh bg-black" />
+</main>
+  )
 }
 
-export default Navbar;
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,86 @@
+import { useGSAP } from '@gsap/react';
+import { SplitText } from 'gsap/all';
+import gsap from 'gsap';
+
+
+import react from 'react';
+
+
+
+const Hero = () => {
+
+    useGSAP(() => {
+        const heroSplit = new SplitText('.title', { type: 'chars, words' });
+        const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
+
+        heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
+
+        gsap.from(heroSplit.chars, {
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            opacity: 0,
+            stagger: 0.04,
+        });
+
+        gsap.from(paragraphSplit.lines, {
+            opacity: 0,
+              yPercent: 100,
+                duration: 1.8,
+                ease: 'expo.out',
+                stagger: 0.1,
+                delay: 1,
+        });
+
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '#hero',
+                start: 'top top',
+                end: 'bottom top',
+                scrub: true,
+            }
+        })
+
+        .to('.right-leaf', { y:200 }, 0)
+        .to('.left-leaf', { y: 200 }, 0)
+    }, []);
+
+    return (
+        <>
+        <section id="hero" className="noisy">
+<h1 className="title">The The The</h1>
+
+
+        <img 
+        src="/images/hero-left-leaf.png" 
+        alt="Left Leaf" 
+        className="left-leaf"
+        />
+
+                <img 
+        src="/images/hero-right-leaf.png" 
+        alt="Right Leaf" 
+        className="right-leaf"
+        />
+
+        <div className="body">
+           <div className="content">
+             <div className="space-y-5 hidden md:block">
+                 <p>btw no</p>
+                 <p className="subtitle">
+                    man i dont know what im doing at this point 💀
+                 </p>
+             </div>
+             <div className="view-cocktails">
+                <p className="subtitle">you are the reason earth is in shambles right now ez (also notice how awfully AI generated the icon is 😭)</p>
+            <a href="#cocktails">View Cockroach- I mean Cocktails</a>
+             </div>
+           </div>
+        </div>
+                </section>
+        </>
+    )
+}
+
+export default Hero;
