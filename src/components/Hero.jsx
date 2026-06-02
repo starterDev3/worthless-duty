9f91022df0fa1455371cef35b64e2c724a692248
starterDev3 <crazyfam@outlook.com>
2026-06-02 17:06:17 -0500

    video thing woohoo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,17 @@
 import { useGSAP } from '@gsap/react';
 import { SplitText } from 'gsap/all';
 import gsap from 'gsap';
-
+import { useMediaQuery } from 'react-responsive';
 
 import react from 'react';
-
+import { useRef } from 'react';
 
 
 const Hero = () => {
+
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({ maxWidth: 767});
 
     useGSAP(() => {
         const heroSplit = new SplitText('.title', { type: 'chars, words' });
@@ -44,6 +48,28 @@ const Hero = () => {
 
         .to('.right-leaf', { y:200 }, 0)
         .to('.left-leaf', { y: 200 }, 0)
+
+
+        const startValue =  isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+
+            }
+        });
+
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            })
+        }
     }, []);
 
     return (
@@ -79,6 +105,17 @@ const Hero = () => {
            </div>
         </div>
                 </section>
+
+<div className="video absolute inset-0">
+<video
+ref={videoRef}
+src="/videos/input.mp4"
+muted
+playsInline
+preload="auto"
+
+/>
+</div>
         </>
     )
 }
